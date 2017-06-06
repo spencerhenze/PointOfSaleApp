5048022614de06573b8368e4f47ef41d26b01981
spenc <spenc@LAPTOP-8LL6OQLE>
2017-06-06 12:14:21 -0600

    Add project files.

---
+++ TeamAwesome.Main/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamAwesome.SubMainMenu;
+
+
+
+
+namespace TeamAwesome.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubMain mySubMain = new SubMain();
+            mySubMain.SubMainMenu();
+        }
+    }
+}
